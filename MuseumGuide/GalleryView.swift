5173f6f045cd683fe4780f37ee6c74629d3620eb
Ralf Ebert <info@ralfebert.de>
2021-09-16 15:21:11 +0200

    endpoints

--- MuseumGuide/GalleryView.swift
+++ MuseumGuide/GalleryView.swift
@@ -1,32 +1,22 @@
 import SwiftUI
 
-struct ArtworksSearchResult: Codable {
-    /// The total number of publicly-available objects
-    var total: Int
-
-    /// An array containing the object ID of publicly-available object
-    var objectIDs: [Int]
-}
-
 @MainActor
 class ArtworkSearchModel: ObservableObject {
     var searchText: String
     @Published var searchResult: ArtworksSearchResult?
+    let endpoints = MetMuseumEndpoints()
 
     init(searchText: String) {
         self.searchText = searchText
     }
 
     func reload() async {
-        let url = URL(string: "https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers")!
-        let urlSession = URLSession.shared
         do {
-            let (data, _) = try await urlSession.data(from: url)
-            self.searchResult = try JSONDecoder().decode(ArtworksSearchResult.self, from: data)
+            self.searchResult = try await self.endpoints.search(query: self.searchText)
         } catch {
             // Error handling in case the data couldn't be loaded
             // For now, only display the error on the console
-            debugPrint("Error loading \(url): \(String(describing: error))")
+            debugPrint("Error loading: \(String(describing: error))")
         }
     }
 }
