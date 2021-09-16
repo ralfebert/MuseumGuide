2491a73d70bc80a3e07a6ea4de96a816c341d0f4
Ralf Ebert <info@ralfebert.de>
2021-09-16 11:32:32 +0200

    empty project

---
+++ MuseumGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MuseumGuide
+//
+//  Created by Ralf Ebert on 16.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MuseumGuide/MuseumGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumGuideApp.swift
+//  MuseumGuide
+//
+//  Created by Ralf Ebert on 16.09.21.
+//
+
+import SwiftUI
+
+@main
+struct MuseumGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
