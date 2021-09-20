47a235ad5de188f03dd06f45ec8ce6728c300873
Ralf Ebert <info@ralfebert.de>
2021-09-20 12:32:00 +0200

    simplified example

--- MuseumGuide/MuseumGuideApp.swift
+++ MuseumGuide/MuseumGuideApp.swift
@@ -1,4 +1,28 @@
+import Foundation
 import SwiftUI
+
+struct Exhibition: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct Artwork: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+class ExhibitionsModel: ObservableObject {
+    @Published var exhibitions = [Exhibition(name: "Picasso"), Exhibition(name: "Van Gogh")]
+}
+
+class ArtistModel: ObservableObject {
+    let name: String
+    @Published var artworks = [Artwork(name: "Artwork 1"), Artwork(name: "Artwork 2")]
+
+    init(name: String) {
+        self.name = name
+    }
+}
 
 @main
 struct MuseumGuideApp: App {
@@ -8,5 +32,36 @@ struct MuseumGuideApp: App {
                 ExhibitionsView()
             }
         }
+    }
+}
+
+struct ExhibitionsView: View {
+    @StateObject var exhibitionsModel = ExhibitionsModel()
+
+    var body: some View {
+        List {
+            ForEach(exhibitionsModel.exhibitions) { exhibition in
+                NavigationLink(
+                    destination: {
+                        ArtistView(artistModel: ArtistModel(name: exhibition.name))
+                    },
+                    label: {
+                        Text(exhibition.name)
+                    }
+                )
+            }
+        }
+        .navigationTitle("Exhibitions")
+    }
+}
+
+struct ArtistView: View {
+    @ObservedObject var artistModel: ArtistModel
+
+    var body: some View {
+        List(artistModel.artworks) { artwork in
+            Text(artwork.name)
+        }
+        .navigationTitle(artistModel.name)
     }
 }
